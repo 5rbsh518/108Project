9f353b50f30fea29d51f88486581d1cfb1e009e9
5rbsh518 <Omerrr350@hotmail.com>
2022-12-10 03:13:04 +0300

    Schedule class

---
+++ src/main/java/com/example/scheduledesigner/Schedule.java
@@ -0,0 +1,9 @@
+package com.example.scheduledesigner;
+
+public class Schedule {
+    Section[] scheduleSections;
+
+    Schedule(Section[] sections){
+        this.scheduleSections = sections;
+    }
+}
