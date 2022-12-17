d15e300125c1e48b4b5afeb6fe46f3fe707158ca
5rbsh518 <Omerrr350@hotmail.com>
2022-12-17 15:30:22 +0300

    Make the schedule an Arraylist

--- src/main/java/com/example/scheduledesigner/HelloApplication.java
+++ src/main/java/com/example/scheduledesigner/HelloApplication.java
@@ -2,22 +2,97 @@ package com.example.scheduledesigner;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import static javafx.application.Application.launch;
+
 public class HelloApplication extends Application {
-    @Override
+
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        //stuff
+        Section[] courseOffering = FileManager.courseOffering2Sections("src/main/java/com/example/scheduledesigner/Data/CourseOffering.csv");
+        Student student = FileManager.FinishedCourses2Student("src/main/java/com/example/scheduledesigner/Data/FinishedCourses.csv");
+        Course[] degreePlan = FileManager.degreePlan2Courses("src/main/java/com/example/scheduledesigner/Data/DegreePlan.csv");
+        Section[] Registrable = FileManager.registrableSections(courseOffering, student, degreePlan);
+
+
+        //first Scene
+
+        BorderPane borderPane = new BorderPane();
+
+        borderPane.setPadding(new Insets(10,10,10,10));
+        Label loadScheduleLabel = new Label("Add sections to Basket");
+        loadScheduleLabel.setFont(new Font("Arial",30));
+        loadScheduleLabel.setPadding(new Insets(5,250,5,5));
+
+
+        //Load schedule button
+        Button loadSchedule = new Button("Start with a saved schedule");
+        loadSchedule.setPadding(new Insets(5,5,5,5));
+
+        HBox top = new HBox();
+        top.setPadding(new Insets(15,15,15,15));
+        top.setAlignment(Pos.CENTER);
+        top.getChildren().addAll(loadScheduleLabel,loadSchedule);
+        borderPane.setTop(top);
+
+        //Pane for registrable courses pane to add and remove
+        ScrollPane courseOfferingPane = new ScrollPane();
+        borderPane.setCenter(courseOfferingPane);
+        courseOfferingPane.setPadding(new Insets(5,5,5,5));
+        VBox vBox = new VBox();
+        for (int i=0; i < Registrable.length;i++){
+            Button btn1 = new Button();
+            Button btn2 = new Button();
+            btn1.setText("add");
+            btn2.setText("remove");
+            HBox hBox = new HBox();
+            HBox btns = new HBox();
+            hBox.setSpacing(500);
+            btns.setSpacing(10);
+
+            btns.getChildren().addAll(btn1,btn2);
+            Label label = new Label(Registrable[i].toString());
+            hBox.getChildren().addAll(label,btns);
+
+            vBox.getChildren().add(hBox);
+
+
+        }
+        courseOfferingPane.setContent(vBox);
+
+
+        //Next scene button
+        Button next = new Button("Next");
+        next.setPadding(new Insets(5,5,5,5));
+        borderPane.setBottom(next);
+
+
+
+
+        Scene scene1 = new Scene(borderPane);
+        stage.setHeight(1000);
+        stage.setWidth(1500);
+        stage.setScene(scene1);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
     }
 }
+
--- src/main/java/com/example/scheduledesigner/Schedule.java
+++ src/main/java/com/example/scheduledesigner/Schedule.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Schedule implements Serializable {
-    private ArrayList<Section> scheduleSections;
+    private ArrayList<Section> scheduleSections = new ArrayList<>();
 
     Schedule(){
 
@@ -25,5 +25,33 @@ public class Schedule implements Serializable {
     }
     public void removeSection(Section section){
         scheduleSections.remove(section);
+    }
+    public boolean addable(Section section){
+        return addableCourse(section) && addableTime(section);
+    }
+
+    private boolean addableCourse(Section section){
+        for(Section scheduleSection: scheduleSections){
+            if(scheduleSection.getCourse().equals(section.getCourse())){
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean addableTime(Section section){
+        if(section.getDays().equals("None") || (section.getTime()[0] == 0 &&section.getTime()[1] == 0 )){
+            return true;
+        }
+        for(Character day: section.getDays().toCharArray()){
+            for(Section scheduleSection: scheduleSections){
+                if(scheduleSection.getDays().contains(day.toString())){//If this section conflict in day with another section check if there is also a time confliction.
+                    if((scheduleSection.getTime()[0] <= section.getTime()[0] && section.getTime()[0] <= scheduleSection.getTime()[1]) || (scheduleSection.getTime()[0] <= section.getTime()[1] && section.getTime()[1] <= scheduleSection.getTime()[1])){
+                        return false;//There is a conflict
+                    }
+                }
+
+            }
+        }
+        return true;
     }
 }
